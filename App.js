987444cf52f9eb51b7e47100859c80498ed6eac3
anjaliiyer12 <anjali.iyer@csu.fullerton.edu>
2020-12-24 13:16:27 +0400

    My First React Native App

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sup Adu!</Text>
+      <Text style={styles.text}>HELLO WORLD!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color:'white',
+    fontWeight:'bold',
+  }
+});
