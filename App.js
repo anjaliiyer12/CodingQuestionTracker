cc2c5ab64db9d274eea55013816b88cabc2d0a85
anjaliiyer12 <anjali.iyer@csu.fullerton.edu>
2020-12-26 15:24:46 +0400

    Basic React Native App Boilerplate

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Sup Adu!</Text>
-      <Text style={styles.text}>HELLO WORLD!</Text>
+      <Text style={styles.text}>Welcome To My First React Native App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,12 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color:'white',
+    color:'pink',
     fontWeight:'bold',
+    fontSize: 20
   }
 });
